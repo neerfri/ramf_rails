dbeaa60f0faa9533d01f1f7de5e6655079b5cc39
Neer Friedman <neer@neer-laptop.(none)>
2008-09-19 20:45:57 +0300

    checking for RAILS_ROOT env variable

--- install.rb
+++ install.rb
@@ -3,7 +3,13 @@ begin
   require 'fileutils'
   overwrite = true
   
-  puts "Installing RAMF"
+  puts "Installing RAMF..."
+  
+  puts "RAILS_ROOT:#{RAILS_ROOT}"
+  
+#  if !File.exist?('./config/initializers/ramf.rb')
+#    FileUtils.copy_file("./vendor/plugins/rubyamf/rails_installer_files/rubyamf_config.rb", "./config/rubyamf_config.rb", false)
+#  end
   
 rescue Exception=>e
   puts "Error installing RAMF rails plugin.:"
