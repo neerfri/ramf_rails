a53857f382c66b53396d6fa24d952be5a92a9f07
Neer Friedman <neer@neer-laptop.(none)>
2008-09-22 16:46:18 +0300

    improvements to ramf_controller.rb. it will now detect amf requests and respond with amf answer

--- installer/ramf_controller.rb
+++ installer/ramf_controller.rb
@@ -1,14 +1,16 @@
 class RamfController < ActionController::Base
   
+  before_filter {|c| c.format=:amf if /x-amf/ =~ c.request.env['CONTENT_TYPE']}
+  
   def gateway
     respond_to do |format|
-      format.html {render :layout=>false}
-      format.amf { }
+      format.html
+      format.amf do
+        amf = RAMF::Rails::Gateway.new.process(request,response)
+        render :text=>amf.inspect
+      end
     end
   end
   
-#  def rescue_action(e)
-#    raise e
-#    #TODO: handle thrown exceptions...
-#  end
+  
 end
