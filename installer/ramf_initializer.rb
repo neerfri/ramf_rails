3813121ad9cdbd77f66cf208863fa898766ef165
Neer Friedman <neer@neer-laptop.(none)>
2008-09-25 12:29:22 +0300

    added configuration parameter SET_AR_NIL_ASSOCIATIONS

--- installer/ramf_initializer.rb
+++ installer/ramf_initializer.rb
@@ -1,2 +1,10 @@
 #register the amf mime-type:
 Mime::Type.register("application/x-amf", :amf) unless defined?(Mime::AMF)
+
+
+#RAMF configuration:
+
+#====Active Record Assosications Null Assignment
+#this option controls wheter to assign associations coming from AMF when the association is set to null.
+#Default is false
+#RAMF::Configuration.SET_AR_NIL_ASSOCIATIONS = false
