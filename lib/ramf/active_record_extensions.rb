bb63a966950861f22758113eefc1b2b3c864dc48
Neer Friedman <neer@neer-laptop.(none)>
2008-09-25 12:30:00 +0300

    implemented ActiveRecord specific flex_members_writer

--- lib/ramf/active_record_extensions.rb
+++ lib/ramf/active_record_extensions.rb
@@ -13,6 +13,17 @@ module RAMF::ActiveRecordExtensions
         instance.class.column_names.include?(member.to_s) ? instance[member] : instance.send(member)
       end
       
+      flex_members_writer do |obj, key, value|
+        if obj.class.column_names.include?(key.to_s)
+          #the member is a column in the table
+          obj.send("#{key}=", value) unless value.is_a?(Float) && value.nan?
+        elsif (reflection = obj.class.reflect_on_association(key))
+          unless !RAMF::Configuration.SET_AR_NIL_ASSOCIATIONS && value.nil?
+            obj.send("#{key}=", value)
+          end
+        end
+      end
+      
     end
   end
   
