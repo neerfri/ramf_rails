e3f0f9803e07848a50702be4432b0c4f5f1b2afb
Neer Friedman <neer@neer-laptop.(none)>
2008-10-14 16:35:54 +0200

    adapted gateway to the new ramf

--- lib/ramf/rails/gateway.rb
+++ lib/ramf/rails/gateway.rb
@@ -1,11 +1,12 @@
 class RAMF::Rails::Gateway
   
   def process(request, response)
+    RAMF::OperationProcessorsManger.add_operation_processor(RAMF::Rails::LoginProcessor)
+    RAMF::OperationProcessorsManger.add_operation_processor(RAMF::Rails::ActionProcessor)
     @request, @response = request, response
     @request_amf = RAMF::Deserializer::Base.new(request.body).process
-    @response_amf = RAMF::AMFObject.new(:version=>@request_amf.version)
-    @request_amf.messages.each {|amf_m| @response_amf.add_message(invoke_action(amf_m))}
-    RAMF::Serializer::Base.new(3).write(@response_amf, @scope)
+    @response_amf = @request_amf.process
+    RAMF::Serializer::Base.new.write(@response_amf, @scope || RAMF::Configuration::DEFAULT_SCOPE)
   end
   
   def invoke_action(amf_message)
@@ -47,14 +48,48 @@ class RAMF::Rails::Gateway
                            :value=>service.render_amf
     end
   end
-  
-  def service_class_name(target_uri)
-    uris = target_uri[0,target_uri.rindex(".")].split(".").map(&:camelize)
-    uris.push("#{uris.pop}Controller") unless uris.last.ends_with?("Controller")
-    uris.join("::")
+end
+
+class RAMF::Rails::ActionProcessor
+  def self.will_process?(operation)
+    #handle any regular remoting_message and any non messaging amf requests
+    operation.remoting_message? || !operation.messaging?
   end
   
-  def action_name(target_uri)
-    target_uri[target_uri.rindex(".")+1..-1].underscore
+  def self.process(operation)
+    path = "/#{operation.service.gsub("Controller","").gsub("::","/").underscore}/#{operation.method}"
+    req = @request.clone
+    res = @response.clone
+    req.env["PATH_INFO"] = req.env["REQUEST_PATH"] = req.env["REQUEST_URI"] = path
+    req.env['HTTP_ACCEPT'] = 'application/x-amf'
+    begin
+      service = ActionController::Routing::Routes.recognize(req).new
+    rescue ActionController::RoutingError=>e
+      raise(RAMF::Rails::CouldNotLoadService, "Unable to find class file for #{operation.service}")
+    end
+    if service.private_methods.include?(action)
+      raise(RAMF::Rails::ServiceMethodIsPublic, "The method {#{action}} in class {#{service.class}} is declared as private, it must be defined as public to access it.")
+    elsif !service.public_methods(true).include?(action)
+      raise(RAMF::Rails::MethodNotDefinedInService, "The method {#{action}} in class {#{service.class}} is not declared.")
+    end
+    service.request_amf = @request_amf
+    service.ramf_params = Array(operations.args)
+    service.process(req, res)
+    @scope = service.amf_scope || RAMF::Configuration::DEFAULT_SCOPE
+    if service.exception_happend?
+      raise service.rescued_exception
+    else
+      service.render_amf
+    end
+  end
+end
+
+class RAMF::Rails::LoginProcessor
+  def self.will_process?(operation)
+    operation.messaging? && operation.login?
+  end
+  
+  def self.process(operation)
+    "Logging in #{operation.credentials.inspect}"
   end
 end
