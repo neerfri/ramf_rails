afc8429f62735d95840b26a32e3d35ffa40a8f6b
Neer Friedman <neer@neer-laptop.(none)>
2008-11-03 16:24:52 +0200

    added check whether RAMF gem is available

--- init.rb
+++ init.rb
@@ -1,6 +1,12 @@
 $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'ramf'
-require 'ramf/rails/action_processor'
-ActionController::Base.send(:include, RAMF::ActionControllerExtensions)
-ActiveRecord::Base.send(:include, RAMF::ActiveRecordExtensions) if defined?(ActiveRecord::Base)
+begin
+  RAMF
+  require 'ramf'
+  require 'ramf/rails/action_processor'
+  ActionController::Base.send(:include, RAMF::ActionControllerExtensions)
+  ActiveRecord::Base.send(:include, RAMF::ActiveRecordExtensions) if defined?(ActiveRecord::Base)
+rescue NameError => e
+  warn("WARNING: RAMF gem not found, Stopped loading ramf_rails !")
+end
+
