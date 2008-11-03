80f05573df09a9b1d831935784fa7eb15e53c986
Neer Friedman <neer@neer-laptop.(none)>
2008-11-03 16:01:51 +0200

    require action_processor.rb in initialization

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'ramf'
+require 'ramf/rails/action_processor'
 ActionController::Base.send(:include, RAMF::ActionControllerExtensions)
 ActiveRecord::Base.send(:include, RAMF::ActiveRecordExtensions) if defined?(ActiveRecord::Base)
