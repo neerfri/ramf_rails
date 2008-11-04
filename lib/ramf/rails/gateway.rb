890d6d33b71695912e60a37c2a8733292b49a557
Neer Friedman <neer@neer-laptop.(none)>
2008-11-04 14:29:09 +0200

    fix bug: invalid argument count

--- lib/ramf/rails/gateway.rb
+++ lib/ramf/rails/gateway.rb
@@ -3,7 +3,7 @@ class RAMF::Rails::Gateway
   def process(request, response)
     @request, @response = request, response
     @request_amf = RAMF::Deserializer::Base.new(request.body).process
-    @response_amf = @request_amf.process(self)
+    @response_amf = @request_amf.process(@request, @response, @request_amf)
     RAMF::Serializer::Base.new.write(@response_amf, @scope || RAMF::Configuration::DEFAULT_SCOPE)
   end
   
