f6f1768dad2af2ab81fe3bf8f41be289e9e9ea96
Neer Friedman <neer@neer-laptop.(none)>
2008-11-29 14:36:34 +0200

    fixed require order and dependency check

--- init.rb
+++ init.rb
@@ -2,11 +2,14 @@ $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) ||
 
 begin
   RAMF
-  require 'ramf'
+  require 'ramf/rails'
+  require 'ramf/action_controller_extensions'
+  require 'ramf/active_record_extensions'
+  require 'ramf/configuration'
   require 'ramf/rails/action_processor'
   ActionController::Base.send(:include, RAMF::ActionControllerExtensions)
   ActiveRecord::Base.send(:include, RAMF::ActiveRecordExtensions) if defined?(ActiveRecord::Base)
-rescue NameError => e
-  warn("WARNING: RAMF gem not found, Stopped loading ramf_rails !")
+#rescue NameError => e
+#  warn("WARNING: RAMF gem not found, Stopped loading ramf_rails !")
 end
 
